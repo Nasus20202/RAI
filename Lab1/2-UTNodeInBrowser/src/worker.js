0ff7abb85969916fc68b164191f9a648455d923b
Krzysztof Nasuta <krzysztof@nasuta.dev>
2025-10-09 16:26:14 +0200

    first commit

---
+++ Lab1/2-UTNodeInBrowser/src/worker.js
@@ -0,0 +1,8 @@
+function Worker(name) {
+  this.Introduce = function () {
+    return "Hello I'm " + name;
+  };
+  this.DoJob = function (task) {
+    return "Worker " + name + " does " + task;
+  };
+}
